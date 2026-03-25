acfd84997d26722f3cafe0eaa3aa65b7330a8ba0
rs3184-blip <rs3184@srmist.edu.in>
2026-03-25 09:35:32 +0530

    message

---
+++ OopsBannerApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Check if number is natural
+        if (number <= 0) {
+            System.out.println("Please enter a natural number (greater than 0)");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
+        }
+    }
+}
